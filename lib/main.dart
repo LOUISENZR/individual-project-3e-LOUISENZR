163596f5674315c008335f70fc513a7bd66dccc8
Louise Nazarossa <louisanaza@gmail.com>
2025-09-25 02:47:58 +0700

    chore: update app title to 'Aplikasi Pengeluaran'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Pengeluaran',
+      title: 'APLIKASI PENGELUARAN',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
